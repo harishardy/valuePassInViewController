733743bd44c5ecbf42a35fdd1ba6df3cd7854f88
Harish Hardy <mj.harishardy@gmail.com>
2019-06-01 10:28:12 +0530

    firstCommit

---
+++ valuePassInViewController/viewControllers/masterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  masterViewController.swift
+//  valuePassInViewController
+//
+//  Created by Harish Hardy on 31/05/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class masterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ valuePassInViewController/viewControllers/secondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewController.swift
+//  valuePassInViewController
+//
+//  Created by Harish Hardy on 31/05/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
